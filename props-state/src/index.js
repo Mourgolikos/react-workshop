36f55bbf26d22308d27619fdab584e977515fb8b
Miltos Stamos <mstamos@gmail.com>
2017-05-18 19:08:51 +0300

    Display all synthetic react events

--- props-state/src/index.js
+++ props-state/src/index.js
@@ -3,11 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentEvent: '----'
+    };
+
+    this.update = this.update.bind(this);
+  }
+
+  update(event) {
+    this.setState({currentEvent: event.type})
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React-Workshop</h2>
+        </div>
+        <div>
+          <p>
+            <h2>Current Event Type: {this.state.currentEvent}</h2>
+          </p>
+          <input
+            onKeyPress={this.update}
+            onCopy={this.update}
+            onCut={this.update}
+            onPaste={this.update}
+            onFocus={this.update}
+            onBlur={this.update}
+            onDoubleClick={this.update}
+            onTouchStart={this.update}
+            onTouchMove={this.update}
+            onTouchEnd={this.update}
+          />
         </div>
         <Button
           buttonName='Totos'
