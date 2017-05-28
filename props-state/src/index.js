e99b25452de997fbedeec8b8c22c10a3e6198688
Miltos Stamos <mstamos@gmail.com>
2017-05-28 21:25:29 +0300

    Display button with name via props

--- props-state/src/index.js
+++ props-state/src/index.js
@@ -3,14 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <div className="App-header">
+            <h2>Welcome to React-Workshop</h2>
+          </div>
+          <Button buttonName='Totos'/>
+        </div>
+      );
+    }
+}
+
+// The Button component
+class Button extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React-Workshop</h2>
-        </div>
-      </div>
-    );
+      <button
+        className='button'
+      >
+        {this.props.buttonName}
+      </button>
+    )
   }
 }
 
