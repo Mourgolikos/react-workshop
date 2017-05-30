d2a75d1a6ac872c650b917282ce1e9a6bd757ddc
Miltos Stamos <mstamos@gmail.com>
2017-05-30 23:58:41 +0300

    Add router for Home page

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+const Home = () => <h2>Home Page</h2>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React-Workshop</h2>
+      <Router>
+        <div>
+          <div className="App">
+            <div className="App-header">
+              <h2>Welcome to React-Workshop</h2>
+            </div>
+          </div>
+          <Route path='/' component={Home}/>
         </div>
-      </div>
+      </Router>
     );
   }
 }
